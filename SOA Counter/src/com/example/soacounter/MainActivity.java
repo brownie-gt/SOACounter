81609988a18ad9a8668f73e2cfa950e3a3bc9331
brownie-gt <brownie.gt@gmail.com>
2014-05-25 19:31:21 -0600

    Se agregaron imagenes de botones y contadores

--- SOA Counter/src/com/example/soacounter/MainActivity.java
+++ SOA Counter/src/com/example/soacounter/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.soacounter;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	TextView carView, busView, truckView;
+	int cars = 0, buses = 0, trucks = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		carView = (TextView) findViewById(R.id.textView1);
+		carView.setText("0");
+		busView = (TextView) findViewById(R.id.textView2);
+		busView.setText("0");
+		truckView = (TextView) findViewById(R.id.textView3);
+		truckView.setText("0");
+	}
+
+	public void countCar(View view) {
+		cars++;
+		carView.setText(String.valueOf(cars));
+	}
+
+	public void countBus(View view) {
+		buses++;
+		busView.setText(String.valueOf(buses));
+	}
+
+	public void countTruck(View view) {
+		trucks++;
+		truckView.setText(String.valueOf(trucks));
 	}
 }
--- SOA Counter/src/com/example/soacounter/ReportActivity.java
+++ SOA Counter/src/com/example/soacounter/ReportActivity.java
@@ -18,16 +18,18 @@ public class ReportActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report);
-		
+
 		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
 		Cursor cursor = dbAdapter.getCounts();
-		
-		String[] adapterCols=new String[]{"PLACE"};
-		int[] adapterRowViews=new int[]{android.R.id.text1};
-		
-		SimpleCursorAdapter sca=new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item, cursor, adapterCols, adapterRowViews,0);
+
+		String[] adapterCols = new String[] { "PLACE" };
+		int[] adapterRowViews = new int[] { android.R.id.text1 };
+
+		SimpleCursorAdapter sca = new SimpleCursorAdapter(this,
+				android.R.layout.simple_spinner_item, cursor, adapterCols,
+				adapterRowViews, 0);
 		sca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(sca);
-		
+
 	}
 }
