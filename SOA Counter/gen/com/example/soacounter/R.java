455c76539f01c4388f0ec500e456e522337737dc
brownie-gt <brownie.gt@gmail.com>
2014-06-06 01:03:21 -0600

    icon update

--- SOA Counter/gen/com/example/soacounter/R.java
+++ SOA Counter/gen/com/example/soacounter/R.java
@@ -11,19 +11,33 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bus=0x7f020000;
+        public static final int car=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int truck=0x7f020003;
     }
     public static final class id {
-        public static final int spinner1=0x7f060000;
+        public static final int button1=0x7f060007;
+        public static final int button2=0x7f060008;
+        public static final int button3=0x7f060009;
+        public static final int editText1=0x7f060006;
+        public static final int imageButton1=0x7f060001;
+        public static final int imageButton2=0x7f060003;
+        public static final int imageButton3=0x7f060005;
+        public static final int textView1=0x7f060000;
+        public static final int textView2=0x7f060002;
+        public static final int textView3=0x7f060004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_report=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int title_activity_report=0x7f040002;
+        public static final int button_graph=0x7f040005;
+        public static final int button_reset=0x7f040003;
+        public static final int button_save=0x7f040004;
+        public static final int input_place=0x7f040002;
+        public static final int title_activity_report=0x7f040001;
     }
     public static final class style {
         /** 
